a37a982447ef9d22cb8db85e3fae48f00a718540
SHJohnson <shjohnson413@gmail.com>
2021-09-09 19:00:50 -0700

    Passport Jwt

--- routes/promotionRouter.js
+++ routes/promotionRouter.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const promotionRouter = express.Router();
+const authenticate = require('../authenticate');
 
 promotionRouter.route('/')
 .all((req, res, next) => {
@@ -10,14 +11,14 @@ promotionRouter.route('/')
 .get((req, res) => {
     res.end('Will send all the promotions to you')
 })
-.post((req, res) => {
+.post(authenticate.verifyUser, (req, res) => {
     res.end(`Will add the promotion: ${req.body.name} with description: ${req.body.description}.`)
 })
-.put((req, res) => {
+.put(authenticate.verifyUser, (req, res) => {
     res.statusCode = 403;
     res.end(`PUT operation not supported on /promotions`);
 })
-.delete((req, res) => {
+.delete(authenticate.verifyUser, (req, res) => {
     res.end(`Deleting all promotions.`);
 });
 
@@ -27,19 +28,19 @@ promotionRouter.route('/:promotionId')
     res.setHeader('Content-Type', 'text/html');
     next();
 })
-.get( (req, res) => {
+.get((req, res) => {
     res.end(`Will send details of the promotion: ${req.params.promotionId} to you.`);
 })
-.post((req, res) => {
+.post(authenticate.verifyUser, (req, res) => {
     res.statusCode = 403;
     res.end(`POST operation not supported on /promotions/${req.params.promotionId}.`);
 })
-.put((req, res) => {
+.put(authenticate.verifyUser, (req, res) => {
     res.write(`Updating the promotion: ${req.params.promotionId}. \n`);
     res.end(`Will update the promotion: ${req.body.name}
         with description: ${req.body.description}.`);
 })
-.delete( (req, res) => {
+.delete(authenticate.verifyUser, (req, res) => {
     res.end(`Deleting promotion: ${req.params.promotionId}.`);
 });
 
