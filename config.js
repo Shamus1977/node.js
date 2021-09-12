b03403196f60dd9cfb1eba4cd3e01c454155208f
SHJohnson <shjohnson413@gmail.com>
2021-09-12 14:54:02 -0700

    Facebook Passport

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     "secretKey": "12345-67890-09876-54321",
-    "mongoUrl": "mongodb://localhost:27017/nucampsite"
+    "mongoUrl": "mongodb://localhost:27017/nucampsite",
+    'facebook':{
+        clientId:'284781133101072',
+        clientSecret:'b10ddc0b1edc552120a124727807a1da'
+    }
 }
